853675439e3960a188184c2d02fc1e7f923cd3f4
@VirusFucker <90620251+xmain12@users.noreply.github.com>
2021-11-03 15:45:32 +0530

    Add files via upload

---
+++ helpers.js
@@ -0,0 +1,22 @@
+/* Copyright (C) 2021 ws virus Fucker.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+VIRUS - FUCKER
+*/
+
+function successfullMessage(msg) {
+    return "👩‍🦰 *QUEEN STEFANIE*:  ```" + msg + "```"
+}
+function errorMessage(msg) {
+    return "🚀 *QUEEN STEFANIE*:  ```" + msg + "```"
+}
+function infoMessage(msg) {
+    return "🤖 *QUEEN STEFANIE*:  ```" + msg + "```"
+}
+
+
+module.exports = {
+    successfullMessage,
+    errorMessage,
+    infoMessage
+}
